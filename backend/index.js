85394fa67817f6950ab0f388b0f51ed4185fe79f
Andrey Lapchik <a.lapchik7@gmail.com>
2022-12-13 19:26:42 +0200

    deploy update

--- backend/index.js
+++ backend/index.js
@@ -15,7 +15,7 @@ const server = createServer(app)
 
 const io = new Server(server, {
 	cors: {
-		origin: 'http://localhost:3000',
+		origin: 'https://lapchat.onrender.com',
 		methods: ['GET', 'POST'],
 	},
 });
